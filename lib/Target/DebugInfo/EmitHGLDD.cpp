5c4f2b187e239c8630ccd1eba586e7b7af591dcc
Fabian Schuiki <fabian@schuiki.ch>
2023-06-27 13:53:57 -0700

    WIP

--- lib/Target/DebugInfo/EmitHGLDD.cpp
+++ lib/Target/DebugInfo/EmitHGLDD.cpp
@@ -16,11 +16,15 @@
 
 #define DEBUG_TYPE "di"
 
+using namespace mlir;
 using namespace circt;
 using llvm::SmallMapVector;
 
 struct DIVariable {
   StringAttr name;
+  LocationAttr loc;
+  /// The SSA value representing the value of this variable.
+  Value value = nullptr;
   // TODO: type
   // TODO: source_loc
   // TODO: output_loc
@@ -33,7 +37,7 @@ struct DIHierarchy {
   /// The name of this level of hierarchy.
   StringAttr name;
 
-  SmallVector<DIVariable, 0> variables;
+  SmallVector<DIVariable *, 0> variables;
   SmallVector<DIHierarchy *, 0> children;
 };
 
@@ -53,6 +57,7 @@ struct DebugInfo {
   }
 
   llvm::SpecificBumpPtrAllocator<DIHierarchy> hierarchyAllocator;
+  llvm::SpecificBumpPtrAllocator<DIVariable> variableAllocator;
 };
 
 DebugInfo::DebugInfo(Operation *op) {
@@ -66,9 +71,30 @@ DebugInfo::DebugInfo(Operation *op) {
     if (auto moduleOp = dyn_cast<hw::HWModuleOp>(op)) {
       auto &hierarchy = getOrCreateHierarchyForModule(moduleOp.getNameAttr());
       hierarchy.op = op;
+
+      // Add variables for each of the ports.
+      auto outputValues =
+          moduleOp.getBodyBlock()->getTerminator()->getOperands();
+      for (auto &port : moduleOp.getAllPorts()) {
+        auto value = port.isOutput() ? outputValues[port.argNum]
+                                     : moduleOp.getArgument(port.argNum);
+        auto *var = new (variableAllocator.Allocate()) DIVariable;
+        var->name = port.name;
+        var->loc = port.loc;
+        hierarchy.variables.push_back(var);
+      }
     } else if (auto instOp = dyn_cast<hw::InstanceOp>(op)) {
-      auto &subhierarchy =
+      auto parentModule = op->getParentOfType<hw::HWModuleOp>();
+      if (!parentModule)
+        return;
+      auto &parentHierarchy =
+          getOrCreateHierarchyForModule(parentModule.getNameAttr());
+      auto &childHierarchy =
           getOrCreateHierarchyForModule(instOp.getModuleNameAttr().getAttr());
+      parentHierarchy.children.push_back(&childHierarchy);
+
+      // TODO: What do we do with the port assignments? These should be tracked
+      // somewhere.
     }
   });
 }
@@ -76,25 +102,41 @@ DebugInfo::DebugInfo(Operation *op) {
 static void encodeLoc(llvm::json::OStream &json, FileLineColLoc loc) {
   // TODO: Collect this into a `file_info` structure up front.
   json.attribute("file", loc.getFilename().getValue());
-  if (auto line = loc.getLine())
-    json.attribute("line", line);
-  if (auto col = loc.getColumn())
-    json.attribute("col", col);
+  if (auto line = loc.getLine()) {
+    json.attribute("begin_line", line);
+    json.attribute("end_line", line);
+  }
+  if (auto col = loc.getColumn()) {
+    json.attribute("begin_column", col);
+    json.attribute("end_column", col);
+  }
 }
 
-static void encodeBestLoc(llvm::json::OStream &json, Location loc) {
-  // TODO: Be more clever about the location we pick here.
-  loc->walk([&](Location loc) {
-    if (auto fileLoc = dyn_cast<FileLineColLoc>(loc)) {
-      encodeLoc(json, fileLoc);
-      return WalkResult::interrupt();
-    }
-    return WalkResult::advance();
-  });
+static FileLineColLoc findBestLocation(Location loc, bool emitted) {
+  if (auto nameLoc = dyn_cast<NameLoc>(loc)) {
+    bool nameIsEmitted = nameLoc.getName() == "emitted";
+    if (emitted == nameIsEmitted)
+      return findBestLocation(nameLoc.getChildLoc(), false);
+  } else if (auto fusedLoc = dyn_cast<FusedLoc>(loc)) {
+    for (auto innerLoc : fusedLoc.getLocations())
+      if (auto bestInnerLoc = findBestLocation(innerLoc, emitted))
+        return bestInnerLoc;
+  } else if (auto fileLoc = dyn_cast<FileLineColLoc>(loc)) {
+    if (!emitted)
+      return fileLoc;
+  }
+  return {};
+}
+
+static void findAndEncodeLoc(llvm::json::OStream &json, StringRef fieldName,
+                             Location loc, bool emitted) {
+  if (auto fileLoc = findBestLocation(loc, emitted))
+    json.attributeObject(fieldName, [&] { encodeLoc(json, fileLoc); });
 }
 
 LogicalResult debug::emitHGLDD(ModuleOp module, llvm::raw_ostream &os) {
   DebugInfo di(module);
+
   llvm::json::OStream json(os, 2);
   json.objectBegin();
 
@@ -105,14 +147,28 @@ LogicalResult debug::emitHGLDD(ModuleOp module, llvm::raw_ostream &os) {
   });
 
   json.attributeArray("objects", [&] {
-    for (auto [moduleNameAttr, hierarchy] : di.moduleNodes) {
+    for (auto moduleNodeEntry : di.moduleNodes) {
+      StringAttr moduleNameAttr;
+      DIHierarchy *hierarchy;
+      std::tie(moduleNameAttr, hierarchy) = moduleNodeEntry;
       json.objectBegin();
-      json.attribute("obj_kind", "module");
+      json.attribute("kind", "module");
+      json.attribute("obj_name", moduleNameAttr.getValue());
       // TODO: This should probably me `sv.verilogName`.
-      json.attribute("hdl_module", moduleNameAttr.getValue());
-      if (auto *op = hierarchy->op)
-        json.attributeObject("hgl_loc",
-                             [&] { encodeBestLoc(json, op->getLoc()); });
+      json.attribute("module_name", moduleNameAttr.getValue());
+      if (auto *op = hierarchy->op) {
+        findAndEncodeLoc(json, "hgl_loc", op->getLoc(), false);
+        findAndEncodeLoc(json, "hdl_loc", op->getLoc(), true);
+      }
+      json.attributeArray("port_vars", [&] {
+        for (auto *var : hierarchy->variables) {
+          json.objectBegin();
+          json.attribute("var_name", var->name.getValue());
+          findAndEncodeLoc(json, "hgl_loc", var->loc, false);
+          findAndEncodeLoc(json, "hdl_loc", var->loc, true);
+          json.objectEnd();
+        }
+      });
       json.objectEnd();
     }
   });
